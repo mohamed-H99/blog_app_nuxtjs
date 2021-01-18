6c9e7fe1b127e05abbaafb18474090204ed40f7a
Ahmed Hashem <47692286+mohamed-H99@users.noreply.github.com>
2021-01-18 19:47:09 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 export default {
+  buildDir: 'nuxt-dist',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'blogApp',
@@ -8,7 +9,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Simple nuxtjs blog app' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
